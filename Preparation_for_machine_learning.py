8cdcac1bd25e8cc5f45c8a1b2601a0ddf3ac8351
douhuanmin <2271177204@qq.com>
2023-09-01 23:50:05 +0800

    2023-09-01

--- Preparation_for_machine_learning.py
+++ Preparation_for_machine_learning.py
@@ -12,3 +12,17 @@ print(data.keys())
 # 1,data.drop("keys",axis=1) # according to column
 data = data.dropna(subset=["ocean_proximity"]) #
 print(data.keys())
+data = data.drop("ocean_proximity",axis=1)
+print(data.keys())
+
+# fill uncertain value with sklearn
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(strategy="median")
+imputer.fit(data)
+# inputer save every median in statistics_
+print(imputer.statistics_ == data.median().values)
+# transform uncertain value into median
+X = imputer.transform(data) # retrun a numpy.array
+print(type(X))
+# if one want to go back to DataFrame
+pd_data = pd.DataFrame(X,columns=data.columns,index=data.index)
