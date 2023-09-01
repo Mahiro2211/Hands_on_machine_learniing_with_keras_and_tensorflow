daa040b32b3e3626f22a755ae67ed90b0368fe18
douhuanmin <2271177204@qq.com>
2023-09-01 23:21:15 +0800

    2023-09-01

--- Creat_Testset_StartifiedShuffle.py
+++ Creat_Testset_StartifiedShuffle.py
@@ -22,8 +22,33 @@ print(start_train_set["income_cat"].value_counts() / len(start_train_set))
 
 #Ding visualization
 
-cp_data = start_test_set.copy()
+cp_data = start_train_set.copy()
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('WebAgg')
-cp_data.plot(kind="scatter",x="longitude",y="latitude")
+# matplotlib.use('WebAgg')
+print(cp_data.keys())
+# cp_data.plot(kind="scatter",x="longitude",y="latitude",alpha=0.1)
+cp_data.plot(kind="scatter",x="longitude",y="latitude",alpha=0.4,
+             s=cp_data["population"],label="population",figsize=(10,7),
+             c="median_house_value",colorbar=True)
+# plt.show()
+# plt.legend()
+
+# find relation between each keys
+
+for key in cp_data.keys():
+    types = cp_data[key].dtype
+    print(f'{key} type is {types}')
+    if types != 'float64':
+        print(cp_data[key])
+print(type(cp_data))
+cp_data = cp_data.drop('ocean_proximity',axis=1)
+corr_matrix = cp_data.corr()
+
+print(corr_matrix["median_house_value"].sort_values(ascending=False))
+
+from pandas.plotting import scatter_matrix
+attributes = ["median_house_value","median_income","total_rooms","housing_median_age"]
+scatter_matrix(cp_data[attributes],figsize=(12,8))
+cp_data.plot(kind="scatter",x="median_income",y="median_house_value",alpha=0.1)
+plt.show()
